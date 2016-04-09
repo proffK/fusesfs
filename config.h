28118178838258b527790279d69141dc56d9b27a
Klim Kireev <edvmartell@gmail.com>
2016-04-10 00:08:46 +0300

    Initial configure commit

---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "sfs-utils"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "proffk@linux.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "sfs-utils"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "sfs-utils 0.01"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "sfs-utils"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.01"
+
+/* Version number of package */
+#define VERSION "0.01"
---
+++ src/mkfs/main.c
@@ -0,0 +1,10 @@
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main() {
+
+        return EXIT_SUCCESS;
+}
