90578c3eccaf9511848591ff985b551e37f2a28f
Klim Kireev <edvmartell@gmail.com>
2016-04-22 18:30:30 +0300

    memcpy was added

--- src/include/sfs/debug.h
+++ src/include/sfs/debug.h
@@ -3,21 +3,21 @@
 #include <stdio.h>
 
 #ifdef SFS_DEBUG
-#define SFS_TRACE(...)     {                           \
+#define SFS_TRACE(...)    do {                           \
                 fprintf(stderr, "SFS   %s:%4d[%s] : ", \
                         __FILE__, __LINE__, __func__); \
                 fprintf(stderr, __VA_ARGS__);          \
-                fprintf(stderr, "\n\r"); }
+                fprintf(stderr, "\n\r"); } while(0)
 #else
 #define SFS_TRACE(...)
 #endif
 
 #ifdef IO_DEBUG
-#define IO_TRACE(...)     {                              \
+#define IO_TRACE(...)   do {                              \
                 fprintf(stderr, "SFS IO  %s:%4d[%s] : ", \
                         __FILE__, __LINE__, __func__);   \
                 fprintf(stderr, __VA_ARGS__);            \
-                fprintf(stderr, "\n\r"); }
+                fprintf(stderr, "\n\r"); } while(0);
 #else
 #define IO_TRACE(...)
 #endif
