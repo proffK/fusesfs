eaa809597b0b0a56f063896581a42ce261273e15
edgar <edos.edgar@gmail.com>
2016-05-05 00:01:48 +0300

    Sfs_init was fixed

--- src/lib/sfs/sfs_init.c
+++ src/lib/sfs/sfs_init.c
@@ -23,20 +23,25 @@ int sfs_init(sfs_unit* fs, blockdev* bdev)
 
         SFS_TRACE("Read MBR fields from image");
         /* Read data_area_size */ 
-        read_data(bdev, offsetof(struct mbr_t, data_area_size), 
-                  (uint8_t*)(&data_area_size), sizeof(data_area_size));
+        if (read_data(bdev, offsetof(struct mbr_t, data_area_size), 
+                  (uint8_t*)(&data_area_size), sizeof(data_area_size)) == -1)
+                return -1;
         /* Read index area size */
-        read_data(bdev, offsetof(struct mbr_t, index_area_size), 
-                  (uint8_t*)(&index_area_size), sizeof(index_area_size));
+        if (read_data(bdev, offsetof(struct mbr_t, index_area_size), 
+                  (uint8_t*)(&index_area_size), sizeof(index_area_size)) == -1)
+                return -1;
         /* Read magic number */
-        read_data(bdev, offsetof(struct mbr_t, magic_number),
-                  (uint8_t*)(magic_number), 3);
+        if (read_data(bdev, offsetof(struct mbr_t, magic_number),
+                  (uint8_t*)(magic_number), 3) == -1)
+                return -1;
         /* Read total size */
-        read_data(bdev, offsetof(struct mbr_t, total_size), 
-                  (uint8_t*)(&total_size), sizeof(total_size));
+        if (read_data(bdev, offsetof(struct mbr_t, total_size), 
+                  (uint8_t*)(&total_size), sizeof(total_size)) == -1)
+                return -1;
         /* Read block size */
-        read_data(bdev, offsetof(struct mbr_t, block_size),
-                  (uint8_t*)(&block_size), sizeof(b_size_p2));
+        if (read_data(bdev, offsetof(struct mbr_t, block_size),
+                  (uint8_t*)(&block_size), sizeof(b_size_p2)) == -1)
+                return -1;
 
         block_size <<= b_size_p2;
         reserved_size *= block_size;
@@ -46,12 +51,14 @@ int sfs_init(sfs_unit* fs, blockdev* bdev)
         fs->del_begin = 0;
 
         /* Read timestamp */
-        read_data(bdev, offsetof(vol_ident_entry, time_stamp) + fs->entry_start,
-                  (uint8_t*)&(fs->time), sizeof(fs->time));
+        if (read_data(bdev, offsetof(vol_ident_entry, time_stamp) + 
+                            fs->entry_start,
+                      (uint8_t*)&(fs->time), sizeof(fs->time)) == -1)
+                return -1;
         /* Write zero to time stamp */
-        write_data(bdev, offsetof(vol_ident_entry, time_stamp) + 
-                   fs->entry_start,
-                   (uint8_t*)&(new_timestamp), sizeof(new_timestamp));
-
+        if (write_data(bdev, offsetof(vol_ident_entry, time_stamp) + 
+                             fs->entry_start,
+                       (uint8_t*)&(new_timestamp), sizeof(new_timestamp)) == -1)
+                return -1;
         return 0;
 }
