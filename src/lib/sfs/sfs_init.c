8a6a6e0f03dcaee4de10b6063c551f66293a5583
edgar <edos.edgar@gmail.com>
2016-05-05 00:49:26 +0300

    SFS_init was fixed

--- src/lib/sfs/sfs_init.c
+++ src/lib/sfs/sfs_init.c
@@ -15,7 +15,7 @@ int sfs_init(sfs_unit* fs, blockdev* bdev)
         uint64_t index_area_size = 0;
         uint8_t magic_number[3];
         uint64_t total_size = 0;
-        uint64_t reserved_size = 0;
+        uint32_t reserved_size = 0;
         uint8_t b_size_p2 = 0;
         uint64_t block_size = 128;
         //uint8_t checksum = 0;
@@ -30,6 +30,10 @@ int sfs_init(sfs_unit* fs, blockdev* bdev)
         if (read_data(bdev, offsetof(struct mbr_t, index_area_size), 
                   (uint8_t*)(&index_area_size), sizeof(index_area_size)) == -1)
                 return -1;
+        /* Read reserved area size */
+        if (read_data(bdev, offsetof(struct mbr_t, reserved_size), 
+                  (uint8_t*)(&reserved_size), sizeof(uint32_t)) == -1)
+                return -1;
         /* Read magic number */
         if (read_data(bdev, offsetof(struct mbr_t, magic_number),
                   (uint8_t*)(magic_number), 3) == -1)
@@ -40,15 +44,18 @@ int sfs_init(sfs_unit* fs, blockdev* bdev)
                 return -1;
         /* Read block size */
         if (read_data(bdev, offsetof(struct mbr_t, block_size),
-                  (uint8_t*)(&block_size), sizeof(b_size_p2)) == -1)
+                  (uint8_t*)(&b_size_p2), sizeof(b_size_p2)) == -1)
                 return -1;
-
+        SFS_TRACE("Calculate all sizes");
         block_size <<= b_size_p2;
+        SFS_TRACE("Block size: %lu", block_size);
+        SFS_TRACE("RESERVED size: %u", reserved_size); 
         reserved_size *= block_size;
         data_area_size *= block_size;
         fs->entry_start = reserved_size + data_area_size;
         fs->vol_ident = fs->entry_start + index_area_size - INDEX_ENTRY_SIZE;
         fs->del_begin = 0;
+        fs->bdev = bdev;
 
         /* Read timestamp */
         if (read_data(bdev, offsetof(vol_ident_entry, time_stamp) + 
